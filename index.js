f1d836af9ea0f2372d84ad1384f8d463c07219e2
solale90 <solaleh.rm@gmail.com>
2018-10-07 22:54:09 -0700

    Modify RandomBlue function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-setInterval(()=>{
+setInterval(() => {
     app.style.backgroundColor = "rgb(255,222,255)";
 }, 500);
 
 //B
-function RandomBlue(){
-
+function RandomBlue() {
+    var blue = Math.round(Math.random() * 255);
+    return "rgb(100, 100, " + blue + ")";
 }
